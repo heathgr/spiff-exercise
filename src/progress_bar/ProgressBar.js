48fb9dc37d314d7acf113932524ab8fdd79f3d7a
Grant Heath <heathgr@gmail.com>
2020-07-10 15:34:36 -0600

    TIME!! everything after this commit will have been done after the alloted time.

--- src/progress_bar/ProgressBar.js
+++ src/progress_bar/ProgressBar.js
@@ -1,6 +1,10 @@
 import React    from "react"
 import Exercise from "../exercise/Exercise"
 
+import useService from './hooks/useService'
+import ExampleService, { Status } from "./services/ExampleService"
+import './ProgressBar.scss'
+
 const ProgressBar = () => {
   return (
     <div className="parser">
@@ -17,8 +21,26 @@ export default ProgressBar
 
 // ----------------------------------------------------------------------------------
 
+const service = new ExampleService()
+
 const Solution = () => {
+
+  const statusService = useService(service)
+  const { status, percentageComplete } = statusService
+
   return (
-    <div>Add solution here</div>
+    <div className="progressBar">
+      <div>{`${Math.round(percentageComplete) * 100}%`}</div>
+      <div className="bar" style={{ width: `${100 * percentageComplete}%` }}></div>
+      {
+        status === Status.IDLE && <button onClick={() => service.start()}>Start Request</button>
+      }
+      {
+        (status === Status.WORKING || status === Status.COMPLETING) && <button disabled >Loading...</button>
+      }
+      {
+        status === Status.WORKING && <button className="finish-request" onClick={() => service.stop()}>Finish Request</button>
+      }
+    </div>
   )
 }
